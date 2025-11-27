7ef346e895a1c808f79330a0da955af365530e4a
DualDura <jdsan21@koreatech.ac.kr>
2025-11-28 03:49:09 +0900

    [Gold IV] Title: N-Queen, Time: 4768 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+typedef pair<int, int> coord; // y, x
+
+bool CheckNextLoc(const vector<coord>& queensLoc, coord loc) {
+	for (size_t i = 0; i < queensLoc.size(); i++) {
+		if (queensLoc[i].first == loc.first || queensLoc[i].second == loc.second) return false; //+ 모양 검사
+		if (queensLoc[i].second - queensLoc[i].first == loc.second - loc.first) return false;  // / 모양 검사
+		if (queensLoc[i].first + queensLoc[i].second == loc.first + loc.second) return false;  // \ 모양 검사
+	}
+
+	return true;
+}
+
+void backtracking(int mapSize, vector<coord>& queensLoc, int nowDepth, int maxDepth, int& cases) {
+	if (nowDepth == maxDepth) {
+		cases++;
+		return;
+	}
+
+	for (int j = 0; j < mapSize; j++) {
+		if (CheckNextLoc(queensLoc, coord(nowDepth, j))) {
+			queensLoc.emplace_back(coord(nowDepth, j));
+			backtracking(mapSize, queensLoc, nowDepth + 1, maxDepth, cases);
+			queensLoc.pop_back();
+		}
+	}
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr); cout.tie(nullptr);
+
+	int n{ 0 };
+	cin >> n;
+
+	int cases{ 0 };
+	vector<coord> queenLocs;
+
+	backtracking(n, queenLocs, 0, n, cases);
+	cout << cases << endl;
+
+	return 0;
+}
