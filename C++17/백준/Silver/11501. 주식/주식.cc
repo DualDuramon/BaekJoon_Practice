5b1111a31eff031b00fad30742863f3845ce76bd
DualDura <jdsan21@koreatech.ac.kr>
2026-02-26 22:42:24 +0900

    [Silver II] Title: 주식, Time: 568 ms, Memory: 16476 KB -BaekjoonHub

--- C++17/백준/Silver/11501. 주식/주식.cc
+++ C++17/백준/Silver/11501. 주식/주식.cc
@@ -16,19 +16,16 @@ void ProbSolving() {
 
 	long sum = 0;
 
-	for (int i = n - 1; i >= 1; i--) {
-		prices[1][i - 1] = max(prices[1][i - 1], prices[1][i]);
-	}
-
-	for (int i = 0; i < n; i++) {
+	int nowMax = prices[0][n - 1];
+	for (int i = n-1 ; i >= 0; i--) {
+		nowMax = max(nowMax, prices[0][i]);
+		prices[1][i] = nowMax;
 		sum += prices[1][i] - prices[0][i];
 	}
 
 
-
 	cout << sum << "\n";
 }
-
 int main(){
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr); cout.tie(nullptr);
